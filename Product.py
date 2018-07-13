a6d998a8ae206a1699ae77d96cbac835b8647b71
Sıla <ay.sila@outlook.com>
2018-07-13 16:43:51 +0300

    product names are more general

--- Product.py
+++ Product.py
@@ -1,10 +1,12 @@
 class Product():
 
-    def  __init__(self):
-        self.product_name = "Milk"
-        self.price = 99.9
-        self.barCode = ""
+    def  __init__(self,product_name,price,barCode):
+        self.product_name = product_name
+        self.price = price
+        self.barCode = barCode
 
 
-p1 = Product ()
+p1 = Product (product_name="Butter",price=10.0,barCode=None)
+p2 = Product (product_name="Milch",price=1.35,barCode=None)
 print('my prıce is', p1.price)
+print('my prıce is', p2.price)
