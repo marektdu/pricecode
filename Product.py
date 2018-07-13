7e9c90c9ccf703cace0d2d8e43358ee14db3c861
başak <gundogdubasakk@gmail.com>
2018-07-13 15:03:15 +0200

    first product features

---
+++ Product.py
@@ -0,0 +1,10 @@
+class Product():
+
+    def  __init__(self):
+        self.product_name = "Milk"
+        self.price = 99.9
+        self.barCode = ""
+
+
+p1 = Product ()
+print('my prıce is', p1.price)
