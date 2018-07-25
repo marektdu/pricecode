8476496a6ce86dc86476fa365fd36063f93bf737
makbulee <makbule05@gmail.com>
2018-07-25 15:45:54 +0300

    addıng water as product

--- Pricecode.py
+++ Pricecode.py
@@ -3,11 +3,13 @@ from Shop import Shop
 
 p1 = Product (product_name="Butter",price=10.0,barCode=None)
 p2 = Product (product_name="Milch",price=1.35,barCode=None)
-print('my prıce is', p1.price)
-print('my prıce is', p2.price)
+p3= Product ("Wasser",1.5, 3057640182693)
 
 s1 = Shop(shop_name= "Milk Shop")
-print('Shop infoold',s1.shop_name,s1.location,len(s1.stock))
 s1.stock.append(p1)
 s1.stock.append(p2)
-print('Shop infonew',s1.shop_name,s1.location,len(s1.stock))
+s1.stock.append(p3)
+
+s2 = Shop("Spaetie")
+p4= Product ("Wasser",2.5, 3057640182693)
+s2.stock.append(p4)
