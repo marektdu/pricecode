312865a6ded3346a27f01813b8e84f278fad5340
Sıla <ay.sila@outlook.com>
2018-07-26 14:51:47 +0300

    Verfügbarkeit

--- Comparator.py
+++ Comparator.py
@@ -1,9 +1,29 @@
+from Shop import Shop
+
 class Comparator ():
 
     def __init__(self):
-        pass
+        self.all_shops = []
 
-    #implement functios here
+
+    def in_stock(self, barcode):
+
+        s1 = Shop("Aldi" )
+        s2 = Shop("Real")
+
+        all_shops_in_range = [s1 , s2]
+
+        products_found = []
+
+        for shop in all_shops_in_range:
+            for product in shop.stock:
+            #  print (pp.product_name)
+                if barcode == product.barCode:
+                    products_found.append((product,shop))
+
+        return products_found
+
+
 
     def add(self, val1, val2):
 
--- someTests.py
+++ someTests.py
@@ -7,18 +7,32 @@ class TestStockMethods(unittest.TestCase):
 
     def test_water_in_stock(self):
 
-        s1 = Shop(shop_name="Milk Shop")
+        s1 = Shop("Aldi")
+        s2 = Shop("Real")
 
-        p1 = Product(product_name="Butter", price=10.0, barCode=None)
-        p2 = Product(product_name="Milch", price=1.35, barCode=None)
+        p1= Product(product_name="Butter", price=10.0, barCode=None)
+        p2 = Product(product_name="Milch", price=0.61, barCode=None)
         p3 = Product("Wasser", 1.5, 3057640182693)
+        p4 = Product("Brot", 2.5, 3057640182693)
 
         s1.stock.append(p1)
         s1.stock.append(p2)
         s1.stock.append(p3)
+        s1.stock.append(p4)
 
-        self.assertEqual(s1.in_stock(12345678),None)
-        self.assertEqual((s1.in_stock(3057640182693)).product_name, 'Wasser')
+        p5 = Product("Butter", 9.8, None)
+        p6 = Product("Milch", 0.65, None)
+        p7 = Product("Wasser", 1.5, 3057640182693)
+        p8 = Product("Brot", 2.5, 3057640182693)
+
+        s2.stock.append(p5)
+        s2.stock.append(p6)
+        s2.stock.append(p7)
+        s2.stock.append(p8)
+
+        self.assertEqual(s1.all_shops_in_range(12345678),None)
+        self.assertEqual((s1.all_shops_in_range(3057640182693)).product_name, 'Wasser')
+
 
 
 class Test_comporator(unittest.TestCase):
